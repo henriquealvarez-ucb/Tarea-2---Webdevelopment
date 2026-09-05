63ed097ea78ccdcfc568dd2d49558d7c5691de75
Henrique Alvarez <henriquealvarezcorralesii@hotmail.com>
2026-09-05 19:18:26 -0400

    implementar listado de jugadores

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { AuthProvider } from "./auth/AuthContext";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import JugadoresList from "./pages/jugadores/JugadoresList";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
 
           <Route element={<ProtectedRoute />}>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/jugadores" element={<JugadoresList />} />
           </Route>
         </Routes>
       </AuthProvider>
